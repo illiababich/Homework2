b1183cd2007b9402b438d498dc02c3b8d7bc982d
illia <stealthcheetah@yandex.by>
2022-01-09 00:40:26 +0200

    tasks added

---
+++ Homework 2/2nd task/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2nd_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
